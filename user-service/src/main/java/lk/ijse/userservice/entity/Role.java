40315ef77616bbe241ebf4e73ee1711cc1a11b35
KokilaDev <kokiladewmini527@gmail.com>
2026-08-15 12:58:10 +0530

    implement user entity

---
+++ user-service/src/main/java/lk/ijse/userservice/entity/Role.java
@@ -0,0 +1,16 @@
+package lk.ijse.userservice.entity;
+
+/**
+ * --------------------------------------------
+ * Author: Kokila Dewmini
+ * GitHub: https://github.com/KokilaDev
+ * --------------------------------------------
+ * Created: 8/15/2026 12:52 PM
+ * Project: SPMS
+ * --------------------------------------------
+ */
+public enum Role {
+    ADMIN,
+    USER,
+    PARKING_MANAGER
+}
---
+++ user-service/src/main/java/lk/ijse/userservice/entity/User.java
@@ -0,0 +1,40 @@
+package lk.ijse.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * --------------------------------------------
+ * Author: Kokila Dewmini
+ * GitHub: https://github.com/KokilaDev
+ * --------------------------------------------
+ * Created: 8/15/2026 12:49 PM
+ * Project: SPMS
+ * --------------------------------------------
+ */
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phoneNumber;
+
+    private Role role;
+}

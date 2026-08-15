06b4ad597cd13c509002ae9da7d9969b2e67b68b
KokilaDev <kokiladewmini527@gmail.com>
2026-08-15 13:03:13 +0530

    implement user repository

---
+++ user-service/src/main/java/lk/ijse/userservice/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package lk.ijse.userservice.repository;
+
+import lk.ijse.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * --------------------------------------------
+ * Author: Kokila Dewmini
+ * GitHub: https://github.com/KokilaDev
+ * --------------------------------------------
+ * Created: 8/15/2026 1:00 PM
+ * Project: SPMS
+ * --------------------------------------------
+ */
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}

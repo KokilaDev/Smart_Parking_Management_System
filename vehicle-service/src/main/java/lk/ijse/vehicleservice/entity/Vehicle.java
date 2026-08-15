52e715c735bbc39c87f560c20f55129abd83d653
KokilaDev <kokiladewmini527@gmail.com>
2026-08-15 18:00:24 +0530

    implement vehicle entity

---
+++ vehicle-service/src/main/java/lk/ijse/vehicleservice/entity/Vehicle.java
@@ -0,0 +1,36 @@
+package lk.ijse.vehicleservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * --------------------------------------------
+ * Author: Kokila Dewmini
+ * GitHub: https://github.com/KokilaDev
+ * --------------------------------------------
+ * Created: 8/15/2026 5:57 PM
+ * Project: SPMS
+ * --------------------------------------------
+ */
+
+@Entity
+@Table(name = "vehicles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String vehicleNumber;
+    private String vehicleType;
+    private String model;
+    private String color;
+    private Long userId;
+
+}
